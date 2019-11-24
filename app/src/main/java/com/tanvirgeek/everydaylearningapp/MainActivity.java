911859ddd39d92259d52de7b24d91d37ce7f9aa5
tanvirgeek <tanvirgeek@gmail.comm>
2019-11-24 13:13:48 +0600

    w14 D2

--- app/src/main/java/com/tanvirgeek/everydaylearningapp/MainActivity.java
+++ app/src/main/java/com/tanvirgeek/everydaylearningapp/MainActivity.java
@@ -5,7 +5,13 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,10 +21,39 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<MainModel> mainModels;
     MainAdapter mainAdapter;
 
+    private EditText number1EditText;
+    private EditText number2EditText;
+    private Button activity2Button;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1EditText = findViewById(R.id.number1_edit_text);
+        number2EditText = findViewById(R.id.number2_edit_text);
+        activity2Button = findViewById(R.id.activity_2_Button);
+
+        activity2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(number1EditText.getText().toString().equals("") || number2EditText.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this,"Please Insert numbers",Toast.LENGTH_SHORT).show();
+                }else {
+                    int number1 = Integer.parseInt(number1EditText.getText().toString());
+                    int number2 = Integer.parseInt(number2EditText.getText().toString());
+                    Intent intentActivity2 = new Intent();
+                    intentActivity2.putExtra("N1", number1);
+                    intentActivity2.putExtra("N2",number2);
+                }
+
+            }
+        });
+
 
         recyclerView = findViewById(R.id.recycler_view);
         //Create Integer Array
